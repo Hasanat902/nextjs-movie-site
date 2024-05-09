4f626e1dfa8476ecf809d6b99f2d9a5d259024cd
Hasanat902 <abulhasanat902@gmail.com>
2024-05-09 11:26:33 +0600

    created about page

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+
+
+const About = () => {
+    return (
+        <div className="max-w-6xl mx-auto space-y-4">
+            <h2 className="text-2xl font-medium text-amber-600">About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nulla modi, tempore voluptates mollitia distinctio labore quam quod cumque eligendi officiis omnis possimus temporibus. Eius provident voluptatem, et quo, reiciendis corrupti, consequatur tempora rem quasi earum a beatae praesentium harum maiores sapiente dicta quidem! Deleniti porro tenetur magnam officia ut.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt labore velit ut quam nostrum eligendi quidem a ea libero distinctio id maiores, architecto et nisi harum perferendis accusantium sequi nulla provident voluptas ducimus. Eos quae autem nostrum mollitia. Ducimus ut reprehenderit provident autem officia, nostrum dignissimos dolorem deserunt quae necessitatibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ipsum voluptatibus quam ad architecto ullam repudiandae, iure, illo, hic ipsa ducimus obcaecati neque voluptas consequatur quo totam ut? Aut, sequi ipsam quas libero aperiam nam velit ad asperiores! Magni consequatur tempore deleniti a temporibus, aliquid eius error voluptas repellat facere.</p>
+        </div>
+    );
+};
+
+export default About;
